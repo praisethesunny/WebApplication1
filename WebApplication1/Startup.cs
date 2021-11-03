82e6a5f9d03d0db62b491f2fdac3ef00de8a59cc
SkyNet_Node_10 <SkyNet_Node_10>
2021-11-03 18:19:39 +0100

    doesn't work as intended

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace WebApplication1
 {
     public class Startup
@@ -23,6 +24,11 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDBContext>(options=>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")))
+            
+            )
+
             services.AddControllersWithViews();
         }
 
