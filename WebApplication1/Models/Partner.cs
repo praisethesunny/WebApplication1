fc083802706a7f9f4bc6578a5e37845c5d0f4fff
SkyNet_Node_10 <SkyNet_Node_10>
2021-11-03 14:50:46 +0100

    Added partner class

---
+++ WebApplication1/Models/Partner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dapper.Core.Entities
+{
+    public class Partner
+    {
+        public int PartnerTypeId { get; set; } 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int PartnerNumber { get; set; }
+        public int CroatianPIN { get; set; }
+        public DateTime CreatedAtUtc { get; set; }
+        public string CreateByUser { get; set; }
+        public bool IsForeign { get; set; }
+        public string ExternalCode { get; set; }
+        public string Gender { get; set; }
+    }
+}
